5085de489cd89492cbdf5dd59329d7c327f56e77
BjarteKnobel <108578939+BjarteKnobel@users.noreply.github.com>
2025-12-29 15:32:24 +0100

    Fix: Remove large-screen font-size reductions in About page logo
    - Removed font-size: 56px override at 1920px breakpoint
    - Removed font-size: 60px override at 2560px breakpoint
    - About page logo now maintains 79px size on large screens to match other navbars
    - Added viewport meta tag to _document.js (Next.js warns this should be in _app.js instead)

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/svg+xml" href="/assets/webpage_icon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
