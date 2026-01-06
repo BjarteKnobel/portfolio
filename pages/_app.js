e88ed5323957fa9b672bc2a9de346f58342fe1b1
BjarteKnobel <108578939+BjarteKnobel@users.noreply.github.com>
2026-01-06 20:33:22 +0100

    Add Vercel Analytics for visitor and page view tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -42,6 +43,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
