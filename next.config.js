ae1e0043841a17aacf92819d5d2e0e8fd04dd0ec
BjarteKnobel <108578939+BjarteKnobel@users.noreply.github.com>
2026-01-12 19:01:49 +0100

    Add rewrites for Vercel Analytics to bypass ad blockers

--- next.config.js
+++ next.config.js
@@ -5,4 +5,12 @@ module.exports = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/_vercel/insights/:path*',
+        destination: 'https://va.vercel-scripts.com/:path*',
+      },
+    ];
+  },
 }; 
